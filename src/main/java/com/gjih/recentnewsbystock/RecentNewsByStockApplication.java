f255159ee6a9377a2247ca9229b6b99e8d99d89e
g-jih <rhakdnj47@gmail.com>
2021-07-08 00:05:44 +0200

    first commit

---
+++ src/main/java/com/gjih/recentnewsbystock/RecentNewsByStockApplication.java
@@ -0,0 +1,13 @@
+package com.gjih.recentnewsbystock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecentNewsByStockApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecentNewsByStockApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gjih/recentnewsbystock/RecentNewsByStockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gjih.recentnewsbystock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecentNewsByStockApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
