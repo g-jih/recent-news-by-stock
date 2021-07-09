2f81c0a11ccb6fcd9d253bc9a119431df1b3fd8e
g-jih <rhakdnj47@gmail.com>
2021-07-09 21:59:09 +0200

    네이버 뉴스 API 추가

--- src/main/java/com/gjih/recentnewsbystock/controller/HomeController.java
+++ src/main/java/com/gjih/recentnewsbystock/controller/HomeController.java
@@ -1,12 +1,20 @@
 package com.gjih.recentnewsbystock.controller;
+import com.gjih.recentnewsbystock.service.NewsService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@RequiredArgsConstructor
 public class HomeController {
 
+    private final NewsService newsService;
+
     @GetMapping("/")
-    public String main() {
+    public String main(Model model) {
+        String news = newsService.getNews("삼성전자");
+        model.addAttribute("news", news);
         return "index";
     }
 
